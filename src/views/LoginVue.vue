b455003b3deabd7cd8a5af4a83c021db1adb1968
shisanlailin@outlook.com <shisanlailin#outlook.com>
2023-03-04 11:06:14 +0800

    feat: 测测测二次侧而出

--- src/views/LoginVue.vue
+++ src/views/LoginVue.vue
@@ -2,7 +2,10 @@
   <div>vue3</div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  const res = ref<boolean>()
+  console.log(res.value ? (res.value == true ? false : true) : false)
 </script>
 
-<style  scoped></style>
+<style scoped></style>
