5e3b2cd9f704cac5be5d5e731edec492c4c1ea6f
zxd <shisanlailin@outlook.com>
2023-04-20 13:39:14 +0800

    fix: 修改音乐文件存放位置🐞

---
+++ dist/assets/useTheme-169d7bf6.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-95821c50.js";import{c as r}from"./@vue-81915dab.js";function a(){const a=o();return{isDark:r((()=>"dark"===a.theme)),changeBack:o=>{a.toggleNowBack(o)}}}export{a as u};
