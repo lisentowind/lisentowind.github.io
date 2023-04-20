8f94c9679af653ad9073a89d63938f4cdb0194c4
zxd <shisanlailin@outlook.com>
2023-04-20 12:56:03 +0800

    feat: 合并gitee版本🚀

---
+++ dist/assets/useTheme-c591ccc8.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-b969b9a2.js";import{c as r}from"./@vue-81915dab.js";function a(){const a=o();return{isDark:r((()=>"dark"===a.theme)),changeBack:o=>{a.toggleNowBack(o)}}}export{a as u};
