70ce0043d854acacf6cee0d60c4dce940f1a3d0b
zxd <shisanlailin@outlook.com>
2023-04-20 14:36:08 +0800

    feat: view-blog-dist提交🌙

---
+++ dist/assets/useTheme-caa4d794.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-60850931.js";import{c as r}from"./@vue-81915dab.js";function a(){const a=o();return{isDark:r((()=>"dark"===a.theme)),changeBack:o=>{a.toggleNowBack(o)}}}export{a as u};
